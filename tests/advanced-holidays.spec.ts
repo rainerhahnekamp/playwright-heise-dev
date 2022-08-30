ca0ce99551b6dc3bf9055a159bddefaf97061c3c
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2022-08-30 23:25:46 +0200

    init

---
+++ tests/advanced-holidays.spec.ts
@@ -0,0 +1,58 @@
+import { test as base, expect, Page } from "@playwright/test";
+
+import { GetBrochure } from "./page-objects/get-brochure";
+
+import { Holidays } from "./page-objects/holidays";
+
+const test = base.extend<{ holidays: Holidays; getBrochure: GetBrochure }>({
+  holidays: async ({ page }, use) => {
+    await use(new Holidays(page));
+  },
+
+  getBrochure: async ({ page }, use) => {
+    await use(new GetBrochure(page));
+  },
+});
+
+test.use({ storageState: "john-list.json" });
+
+test.describe("Advanced Holidays", () => {
+  test.only("sanity test", async ({ page }) => {
+    await page.goto("");
+
+    await expect(page.locator("data-testid=p-username")).toHaveText(
+      "Welcome John List"
+    );
+  });
+  for (const { response, lookupResult } of [
+    { response: [], lookupResult: "Address not found" },
+
+    { response: [true], lookupResult: "Brochure sent" },
+  ]) {
+    test(`should return ${lookupResult} for nominatim response ${response}`, async ({
+      page,
+
+      holidays,
+
+      getBrochure,
+    }) => {
+      await page.goto("");
+
+      await holidays.navigateTo();
+
+      page.route(/nominatim/, (route) =>
+        route.fulfill({
+          status: 200,
+
+          body: JSON.stringify(response),
+        })
+      );
+
+      await holidays.clickGetBrochure("London");
+
+      await getBrochure.setAddress("Domgasse 5");
+
+      await getBrochure.search();
+    });
+  }
+});
